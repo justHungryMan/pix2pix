96a3e52836269007eed2ae9fb2afda39d2c689eb
justHungryMan <justHungryMan@gmail.com>
2020-03-02 18:08:56 +0900

    [DOING] imageloader

---
+++ train.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transform as transform
+import numpy as np
+import argparse
+
+from network import D, G
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
