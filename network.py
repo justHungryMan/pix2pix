cbea245556e7479414d16a2311a27e7689dd4f5e
justHungryMan <justHungryMan@gmail.com>
2020-03-18 07:46:45 +0000

    [CHANGE] Robust use

--- network.py
+++ network.py
@@ -11,43 +11,44 @@ class Generator(nn.Module):
         self.conv128_64 = nn.Sequential(
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(128)
         )
         self.conv64_32 = nn.Sequential(
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(256),
+            nn.BatchNorm2d(256)
         )
         self.conv32_16 = nn.Sequential(
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512)
         )
         self.conv16_8 = nn.Sequential(
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512)
         )
         self.conv8_4 = nn.Sequential(
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512)
         )
         self.conv4_2 = nn.Sequential(
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512)
         )
         self.conv2_1 = nn.Sequential(
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512)
         )
 
         self.conv1_2 = nn.Sequential(
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(512),
+            nn.Dropout2d(0.5, inplace=True)
         )
         self.conv2_4 = nn.Sequential(
             nn.ReLU(inplace=True),
@@ -64,28 +65,26 @@ class Generator(nn.Module):
         self.conv8_16 = nn.Sequential(
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(512 * 2, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
-            nn.Dropout2d(0.5, inplace=True),
+            nn.BatchNorm2d(512)
         )
-        self.conv16_32 = nn.Sequential(
+        self.conv16_32 = nn.Sequential(            
+            nn.ReLU(inplace=True),
             nn.ConvTranspose2d(512 * 2, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True)
+            nn.BatchNorm2d(256)
         )
         self.conv32_64 = nn.Sequential(
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(256 * 2, 128, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(128)
         )
         self.conv64_128 = nn.Sequential(
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(128 * 2, 64, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(64)
         )
         self.conv128_256 = nn.Sequential(
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(64 * 2, 3, kernel_size=4, stride=2, padding=1),
-            #nn.BatchNorm2d(3),
             nn.Tanh()
         )
     def forward(self, x):
@@ -98,13 +97,13 @@ class Generator(nn.Module):
         out4_2 = self.conv4_2(out8_4)
         out2_1 = self.conv2_1(out4_2)
         out1_2 = self.conv1_2(out2_1)
-        out2_4 = self.conv2_4(torch.cat((out1_2, out4_2), 1))
-        out4_8 = self.conv4_8(torch.cat((out2_4, out8_4), 1))
-        out8_16 = self.conv8_16(torch.cat((out4_8, out16_8), 1))
-        out16_32 = self.conv16_32(torch.cat((out8_16, out32_16), 1))
-        out32_64 = self.conv32_64(torch.cat((out16_32, out64_32), 1))
-        out64_128 = self.conv64_128(torch.cat((out32_64, out128_64), 1))
-        out128_256 = self.conv128_256(torch.cat((out64_128, out256_128), 1))
+        out2_4 = self.conv2_4(torch.cat((out4_2, out1_2), 1))
+        out4_8 = self.conv4_8(torch.cat((out8_4, out2_4), 1))
+        out8_16 = self.conv8_16(torch.cat((out16_8, out4_8), 1))
+        out16_32 = self.conv16_32(torch.cat((out32_16, out8_16), 1))
+        out32_64 = self.conv32_64(torch.cat((out64_32, out16_32), 1))
+        out64_128 = self.conv64_128(torch.cat((out128_64, out32_64), 1))
+        out128_256 = self.conv128_256(torch.cat((out256_128, out64_128), 1))
 
         return out128_256
 
